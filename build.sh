c1491e4d4c5e1567d4a3a00a30b1ee2f9df87300
Jackson Furr <jackson.a.furr@gmail.com>
2024-01-13 00:58:20 -0500

    formatting

--- build.sh
+++ build.sh
@@ -7,5 +7,4 @@ go build -o build/server src/server.go &&
 cd build &&
 export HOME_URL=home.html &&
 export ADD_URL=add_workout.html &&
-export URL=postgres://postgres:password@172.17.0.2:5432/workouts &&
 ./server
