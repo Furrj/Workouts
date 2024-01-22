66d59ff5cc393e465cab8220d67cd19be23cd246
Jackson Furr <jackson.a.furr@gmail.com>
2024-01-22 18:48:05 -0500

    dev build script

---
+++ scripts/devbuild.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source set_vars_dev.env &&
+cd "$PROJECT_ROOT" &&
+rm -rf "$BUILD_DIR" &&
+mkdir "$BUILD_DIR" &&
+mkdir "$HTML_DIR" &&
+mkdir "$DATA_DIR" &&
+mkdir "$LOGS_DIR" &&
+echo "workoutID,setID,timestamp,text,name,reps,weights" >> "${DATA_DIR}/data.csv"
+cp src/html/* "$HTML_DIR" &&
+go build -o "$BUILD_DIR"/server.exe src/server.go &&
+cd build &&
+./server.exe
