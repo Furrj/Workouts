859560bc678bbbcbd3ad2e0b2a8e2e7bfc402e93
Jackson Furr <jackson.a.furr@gmail.com>
2024-01-23 18:36:33 -0500

    read into array

--- src/server.go
+++ src/server.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"github.com/Furrj/Workouts/src/internal/dataHandler"
 	"github.com/Furrj/Workouts/src/internal/routeHandler"
 	"github.com/Furrj/Workouts/src/internal/utils"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	rh := routeHandler.NewRouteHandler(utils.InitEnvVars())
+
+	records, err := dataHandler.ReadWorkouts(rh.EnvVars.SetsCsvUrl)
+	if err != nil {
+		fmt.Printf("Error reading data.csv: %+v\n", err)
+		os.Exit(1)
+	}
+	for i, l := range records {
+		fmt.Printf("%d: %+v\n", i, l)
+	}
 
 	http.HandleFunc("/", rh.Home)
 	http.HandleFunc("/add", rh.AddWorkout)
